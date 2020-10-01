947d24a845f831d974ac38eb6822435f68d1ee63
Stishenok92 <Stishenok92@gmail.com>
2020-10-01 21:30:32 +0300

    v 1.0

---
+++ MoodSensorLibrary/MoodSensor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MoodSensorLibrary
+{
+    public class MoodSensor
+    {
+        
+    }
+}
---
+++ MoodSensorProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodSensorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

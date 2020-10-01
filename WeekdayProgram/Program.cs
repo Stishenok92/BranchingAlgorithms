c24bf7009d1e2a72185cd4ea9b3932361300a693
Stishenok92 <Stishenok92@gmail.com>
2020-10-01 22:53:04 +0300

    v 1.0

---
+++ WeekdayProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekdayProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
